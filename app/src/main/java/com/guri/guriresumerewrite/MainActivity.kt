d80337b8ebabb194467ec9790df2d44aa605ee9b
Gurdeep <saini.gurdeep1688@gmail.com>
2025-06-08 20:49:38 +0530

    adding scrolling in column

--- app/src/main/java/com/guri/guriresumerewrite/MainActivity.kt
+++ app/src/main/java/com/guri/guriresumerewrite/MainActivity.kt
@@ -25,7 +25,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
@@ -118,9 +120,9 @@ fun PDFPickerScreen(modifier: Modifier = Modifier) {
     }
 
     Column(
-        modifier = modifier,
+        modifier = modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
     ) {
 
         if (selectedPDFUri != null) {
